f946e0f3c70e963348c2ea717d32f432b86c3f79
anjalivarun <anjalivarun2004@gmail.com>
2026-06-02 21:35:23 +0530

    Web App Completed

---
+++ src/prompt.py
@@ -0,0 +1,8 @@
+system_prompt =(
+    "You are an assistant for question-answering tasks."
+    "Use the following pieces of retrieved context to answer the question"
+    "if you don't know the answer, say you don't know."
+    "use three sentences maximun and the keep the answer concise."
+    "\n\n"
+    "{context}"
+)
