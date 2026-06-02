bfa9ce16269ed995fa68cb5e4a372e89feed9665
anjalivarun <anjalivarun2004@gmail.com>
2026-06-02 14:49:49 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name ="Medical_chatbot",
+    version="0.1.0",
+    author="Anjali Varun",
+    author_email="anjalivarun377@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
